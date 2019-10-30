8c6fc4049ca8b68d278df86ffa12dc0246ddf5e1
yetote <503779938@qq.com>
2019-10-30 17:24:56 +0800

    先占坑
    占坑...

---
+++ app/src/main/java/com/yetote/mp4info/MainActivity.java
@@ -0,0 +1,24 @@
+package com.yetote.mp4info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+    public native String stringFromJNI();
+}
